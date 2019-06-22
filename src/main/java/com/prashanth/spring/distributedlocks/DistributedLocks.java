8f4e6f799b24d5e45bb77c04506dc51afbf16870
prashanth-g <prashanthcsgce@gmail.com>
2019-06-22 17:43:23 +0530

    Added boot class

---
+++ src/main/java/com/prashanth/spring/distributedlocks/DistributedLocks.java
@@ -0,0 +1,11 @@
+package com.prashanth.spring.distributedlocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedLocks {
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedLocks.class, args);
+    }
+}
