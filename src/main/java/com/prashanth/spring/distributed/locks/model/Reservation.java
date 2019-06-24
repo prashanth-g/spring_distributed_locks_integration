46cc2e018aba5a68ffa3e213997d77669b70e5d1
prashanth-g <prashanthcsgce@gmail.com>
2019-06-24 19:24:34 +0530

    Updated Reservation and ReservationRepository

--- src/main/java/com/prashanth/spring/distributed/locks/model/Reservation.java
+++ src/main/java/com/prashanth/spring/distributed/locks/model/Reservation.java
@@ -1,4 +1,14 @@
 package com.prashanth.spring.distributed.locks.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Reservation {
+    private Integer id;
+    private String name;
 }
+
--- src/main/java/com/prashanth/spring/distributed/locks/repository/ReservationRepository.java
+++ src/main/java/com/prashanth/spring/distributed/locks/repository/ReservationRepository.java
@@ -1,5 +1,28 @@
 package com.prashanth.spring.distributed.locks.repository;
 
-public class ReservationRepository {
+import com.prashanth.spring.distributed.locks.model.Reservation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class ReservationRepository {
+    private final RowMapper<Reservation> rowMapper = (rs, i) -> new Reservation(rs.getInt("id"),
+            rs.getString("name"));
+
+    private final JdbcTemplate jdbcTemplate;
+
+    Optional<Reservation> findById(Integer id) {
+        List<Reservation> reservationList = jdbcTemplate.query("select * from reservation where id = ?", this.rowMapper, id);
+        if(reservationList.size() > 0) {
+            return Optional.ofNullable(reservationList.iterator().next());
+        }
+        return Optional.empty();
+    }
 }
