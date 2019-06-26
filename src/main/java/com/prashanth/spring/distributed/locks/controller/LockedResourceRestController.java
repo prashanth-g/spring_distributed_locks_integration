f04648924d03cfc04b7e157441e096e7530bb2b5
prashanth-g <prashanthcsgce@gmail.com>
2019-06-26 22:10:33 +0530

    Added lockresouce rest controller

---
+++ src/main/java/com/prashanth/spring/distributed/locks/controller/LockedResourceRestController.java
@@ -0,0 +1,20 @@
+package com.prashanth.spring.distributed.locks.controller;
+
+import com.prashanth.spring.distributed.locks.model.Reservation;
+import com.prashanth.spring.distributed.locks.repository.ReservationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class LockedResourceRestController {
+
+    private final ReservationRepository reservationRepository;
+
+    @GetMapping("/update/{id}/{name}/{time}")
+    Reservation update(@PathVariable Integer id, @PathVariable String name, @PathVariable Long time) {
+        return null;
+    }
+}
